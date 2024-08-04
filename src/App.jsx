e6142d43b0d92250c007b340f8d440805964e445
KiwiScreams <screamskiwi3@gmail.com>
2024-08-04 15:28:22 +0400

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import star from "./assets/images/icon-star.svg"
 function App() {
 
   return (
     <>
       <section className="container">
+        <div className="star-image">
+          <img src={star} alt="icon-star" />
+        </div>
+        <h2>How did we do?</h2>
+        <p className="text">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+        <div className="numbers-container">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+        </div>
         <button>SUBMIT</button>
       </section>
     </>
